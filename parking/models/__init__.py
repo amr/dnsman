c4715369b797c0dbacbe598aa62005a753dee534
Amr Mostafa <amr.mostafa@gmail.com>
2010-03-26 12:11:05 +0200

    Added an example to the parking page name help

--- parking/models/__init__.py
+++ parking/models/__init__.py
@@ -5,7 +5,7 @@ from django.template.defaultfilters import slugify
 from dnsman.parking.models.fields import DirectoryPathField
 
 class ParkingPage(models.Model):
-    name = models.CharField(max_length=32, unique=True, help_text='Unique human-readable identifier for this parking page')
+    name = models.CharField(max_length=32, unique=True, help_text='Unique human-readable identifier for this parking page, example: "N2V Standard"')
     template = models.TextField(null=True, blank=True, help_text='Edit the parking page XHTML template right in the browser')
     extends = models.ForeignKey('ParkingPage', null=True, blank=True, help_text='You may extend an existing parking page by selecting it. Read more about template inheritance: http://docs.djangoproject.com/en/1.1/topics/templates/#id1')
     resources_dir = DirectoryPathField(path=settings.PARKING_PAGES_DIR, default="[auto]", help_text="The directory which contains the external resources for the page, such as CSS and Images. Choose [auto-create] and a directory will be automatically created. To add a directory manually, you need to place it under %s" % settings.PARKING_PAGES_DIR)
